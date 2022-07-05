765c33799b53a5ec7adea16beeb786cb95afdb6f
luonglv236 <luonglv236@gmail.com>
2022-07-05 15:58:59 +0700

    add custom PROMISE

---
+++ CustomPromise.js
@@ -0,0 +1,48 @@
+const PENDING = 0;
+const FULFILLED = 1;
+const REJECTED = 2;
+
+function CustomPromise(executor) {
+  let state = PENDING;
+  let value = null;
+  let handlers = [];
+  let catches = [];
+
+  function resolve(result) {
+    if(state !== PENDING) return;
+
+    state = FULFILLED;
+    value = result;
+
+    handlers.forEach(h => h(value));
+  }
+
+  function reject(err) {
+    if (state !== PENDING) return;
+
+    state = REJECTED;
+    value = err;
+
+    catches.forEach(c => c(err));
+  }
+
+  this.then = function (callback) {
+    if (state === FULFILLED) {
+      callback(value);
+    } else {
+      handlers.push(callback);
+    }
+  }
+
+  executor(resolve, reject);
+}
+
+const doWork = (res, rej) => {
+  setTimeout(() => { res("Hello Work") }, 1000);
+}
+
+let someText = new CustomPromise(doWork);
+
+someText.then((val) => {
+  console.log("1st log: " + val);
+})
